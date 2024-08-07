520436d193fc4af5f1f5543b7d76cb62ba5d2891
Gordillo4045 <andre.vizuet14@gmail.com>
2024-08-07 13:20:30 -0700

    fix toggle theme

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,18 +3,23 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext<any>(undefined);
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [theme, setTheme] = useState(() => localStorage.getItem("theme") ?? "dark");
+    const [theme, setTheme] = useState(() => {
+        const storedTheme = localStorage.getItem("theme");
+        return storedTheme ? storedTheme : "dark";
+    });
 
     const toggleTheme = () => {
-        const newTheme = localStorage.getItem("theme") == "dark" ? "light" : "dark";
+        const newTheme = theme === "dark" ? "light" : "dark";
         document.querySelector("body")?.classList.replace(theme, newTheme);
         localStorage.setItem("theme", newTheme);
         setTheme(newTheme);
     }
 
     useEffect(() => {
-        document.querySelector("body")?.classList.add(theme, "text-foreground", "bg-background");
-    })
+        const currentTheme = localStorage.getItem("theme") ?? "dark";
+        document.querySelector("body")?.classList.add(currentTheme, "text-foreground", "bg-background");
+        setTheme(currentTheme);
+    }, []);
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
