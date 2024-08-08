22ccfcb9d34f1a599605c468900368a8ac40f796
Gordillo4045 <andre.vizuet14@gmail.com>
2024-08-08 12:27:41 -0700

    remove animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,6 @@ import { useCart } from './CartContext';
 import { FaCartPlus } from "react-icons/fa6";
 import { toast } from 'sonner';
 import { MdShoppingCartCheckout } from 'react-icons/md';
-import { motion, AnimatePresence } from 'framer-motion';
 
 interface ProductCardProps {
   id: string;
@@ -56,104 +55,83 @@ const ProductCard: React.FC<ProductCardProps> = ({ id, nombre, precio, imagen, m
           </Button>
         </CardFooter>
       </Card>
-      <AnimatePresence>
-        {isOpen && (
-          <Modal
-            placement='center'
-            isOpen={isOpen}
-            onOpenChange={onOpenChange}
-            scrollBehavior='inside'
-            motionProps={{
-              variants: {
-                enter: { opacity: 1, y: 0 },
-                exit: { opacity: 0, y: 20 }
-              },
-              transition: { duration: 0.3 }
-            }}
-          >
-            <ModalContent>
-              {(onClose) => (
-                <>
-                  <ModalHeader className="flex flex-col gap-1">
-                    <motion.div
-                      initial={{ opacity: 0, y: -20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ delay: 0.2, duration: 0.5 }}
-                      className="flex flex-col text-base font-normal gap-2"
-                    >
-                      <div className="text-xl font-semibold">{nombre}</div>
-                      <div className="">
-                        <span>Marca: </span>
-                        {marca}</div>
-                      <div className="">
-                        <span>Modelo: </span>
-                        {modelo}</div>
-                    </motion.div>
-                  </ModalHeader>
-                  <ModalBody>
-                    <motion.div
-                      initial={{ opacity: 0, scale: 0.8 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      transition={{ delay: 0.3, duration: 0.5 }}
-                    >
-                      <Image
-                        isBlurred
-                        isZoomed
-                        shadow="sm"
-                        radius="lg"
-                        width="100%"
-                        alt={nombre}
-                        className="max-h-screen"
-                        src={imagen}
-                      />
-                    </motion.div>
-                    <motion.p
-                      initial={{ opacity: 0, x: -20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{ delay: 0.4, duration: 0.5 }}
-                    >
+      {isOpen && (
+        <Modal
+          placement='center'
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          scrollBehavior='inside'
+          motionProps={{
+            variants: {
+              enter: { opacity: 1, y: 0 },
+              exit: { opacity: 0, y: 20 }
+            },
+            transition: { duration: 0.3 }
+          }}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  <div className="text-xl font-semibold">{nombre}</div>
+                  <div className="font-normal text-base">
+                    <span>Marca: </span>
+                    {marca}</div>
+                  <div className="font-normal text-base">
+                    <span>Modelo: </span>
+                    {modelo}</div>
+                </ModalHeader>
+                <ModalBody>
+
+                  <Image
+                    isBlurred
+                    isZoomed
+                    shadow="sm"
+                    radius="lg"
+                    width="100%"
+                    alt={nombre}
+                    className="max-h-screen"
+                    src={imagen}
+                  />
+                  <div className="flex flex-col gap-2">
+                    <span>
                       {`Precio: $${precio.toFixed(2)}`}
-                    </motion.p>
-                    <motion.span
-                      initial={{ opacity: 0, x: -20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{ delay: 0.5, duration: 0.5 }}
-                    >
-                      Existencias: {existencias} piezas
-                    </motion.span>
-                  </ModalBody>
-                  <ModalFooter>
-                    <motion.div
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ delay: 0.6, duration: 0.5 }}
-                      className="flex items-center justify-center gap-4"
-                    >
-                      <span className='font-light text-sm'>Cantidad</span>
-                      <Input
-                        type="number"
-                        variant='underlined'
-                        defaultValue="1"
-                        min="1"
-                        max={existencias}
-                        onChange={(e) => setCantidad(parseInt(e.target.value))}
-                      />
-                      <ButtonGroup>
-                        <Button color="primary" onPress={handleAddToCart}>
-                          Agregar
-                        </Button>
-                        <Button color="danger" variant="light" onPress={onClose}>
-                          Cerrar
-                        </Button>
-                      </ButtonGroup>
-                    </motion.div>
-                  </ModalFooter>
-                </>
-              )}
-            </ModalContent>
-          </Modal>
-        )}
-      </AnimatePresence>
+
+                    </span>
+                    <span>
+
+                      {`Existencias: ${existencias} piezas`}
+                    </span>
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <div
+                    className="flex items-center justify-center gap-4"
+                  >
+                    <span className='font-light text-sm'>Cantidad</span>
+                    <Input
+                      type="number"
+                      variant='underlined'
+                      defaultValue="1"
+                      min="1"
+                      max={existencias}
+                      onChange={(e) => setCantidad(parseInt(e.target.value))}
+                    />
+                    <ButtonGroup>
+                      <Button color="primary" onPress={handleAddToCart}>
+                        Agregar
+                      </Button>
+                      <Button color="danger" variant="light" onPress={onClose}>
+                        Cerrar
+                      </Button>
+                    </ButtonGroup>
+                  </div>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      )}
     </>
   );
 };
