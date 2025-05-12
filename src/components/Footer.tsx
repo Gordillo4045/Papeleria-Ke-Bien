e99bf68c89cfff293a9024e6e5cfde13559ae0df
Gordillo4045 <andre.vizuet14@gmail.com>
2025-05-12 13:24:30 -0700

    fix: update layout and social media links in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,23 @@ import { SiVite, SiTailwindcss } from "react-icons/si";
 
 export default function Footer() {
     return (
-        <div className="grid grid-rows-2 md:grid-rows-none md:grid-flow-col rounded-b-xl">
+        <div className="grid grid-rows-2 md:grid-rows-none md:grid-flow-col rounded-b-xl max-w-7xl mx-auto">
             <div className="p-4 row-span-3">
                 <span className="font-bold">Ubicacion</span>
                 <p className="text-sm">Zona norte, Avenida F, Zona Norte, 22000 Tijuana, B.C.</p>
             </div>
             <div className="rounded-b-xl col-span-2 md:pt-4">
-                <div className="flex items-center place-content-center md:place-content-start self-center gap-3 ">
-                    <a href="https://www.facebook.com/people/Papeleria-Ke-bien/61553994592502/" target="_blank">
+                <div className="flex items-center justify-center place-content-center md:place-content-start self-center gap-3 ">
+                    {/* <a href="https://www.facebook.com/people/Papeleria-Ke-bien/61553994592502/" target="_blank">
+                        <FaFacebookSquare size={"2rem"} color={"blue"} />
+                    </a> */}
+                    <a href="https://www.facebook.com/share/18tF7HWn5c/" target="_blank">
                         <FaFacebookSquare size={"2rem"} color={"blue"} />
                     </a>
-                    <a href="https://wa.me/6631257749" target="_blank">
+                    <a href="https://wa.me/6613503857 " target="_blank">
                         <FaWhatsappSquare size={"2rem"} color={"green"} />
                     </a>
-                    <a href="mailto:pape.kebien@gmail.com" target="_blank">
+                    <a href="mailto:papeleriakebien@gmail.com" target="_blank">
                         <BiLogoGmail size={"2rem"} color={"red"} />
                     </a>
                 </div>
